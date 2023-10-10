17ba3cb24d25409fa447a69b7af7ce0053b38cba
Renato <renato.ferreiraspfc@hotmail.com>
2023-10-10 05:37:15 -0300

    add final stage 2 things

--- final/stage_2/f2_2_altas.py
+++ final/stage_2/f2_2_altas.py
@@ -4,25 +4,28 @@ import rospy
 
 dr = MAV2()
 
-base = (0.6, 0.6, 0)
+bases = [(-2.75, 0, 1.5), (-0.25, 7, 1)]
 
-HOVER_DISTANCE = 1
+HOVER_DISTANCE = 2.5
 WAIT_FOR_DETECTION = 30
 NUM_SHAKE = 4
 
 try:
     dr.set_mode('GUIDED')
 
-    dr.takeoff(1)
+    dr.takeoff(HOVER_DISTANCE)
     rospy.time(7)
+    
+    for base in bases:
+        dr.go_to_local(base[0], base[1], HOVER_DISTANCE)
+        for _ in range(NUM_SHAKE):
 
-    dr.go_to_local(base[0], base[1], base[2] + HOVER_DISTANCE)
-    for _ in range(NUM_SHAKE):
-
-        time.sleep(WAIT_FOR_DETECTION/NUM_SHAKE)
-        print("shaking...")
+            time.sleep(WAIT_FOR_DETECTION/NUM_SHAKE)
+            print("shaking...")
     
 except KeyboardInterrupt:
     dr.go_to_local(0, 0, HOVER_DISTANCE)
     dr.land()
 
+dr.go_to_local(0, 0, HOVER_DISTANCE)
+dr.land()
