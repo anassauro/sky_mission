478537da41ef56d764ee628282b70fe5f7f845e0
leca <100461457+hmoyen@users.noreply.github.com>
2023-10-15 13:04:53 -0300

    debug extra comma

--- deliverables/import csv.py
+++ deliverables/import csv.py
@@ -25,27 +25,20 @@ with open(input_file, mode='r') as input_csv, open(output_file, mode='w', newlin
         # Convert microseconds to seconds
         gps_time_of_week_s = gps_time_of_week_us / 1_000_000_000
 
-        if((gps_time_of_week_s/100000) < 1 and gps_time_of_week_s > 0.1):
+        if (gps_time_of_week_s / 100000) < 1 and gps_time_of_week_s > 0.1:
             gps_time_of_week_s = gps_time_of_week_s * 10
-
 
         # Calculate the GPSTime based on the GPS week number and GPS time of the week in seconds
         gps_time1 = gp.GPSTime(gps_week_number, gps_time_of_week_s)
 
         # Replace the GPS week and time columns with the GPSTime converted to a datetime object
-        row[0] = gps_time1.to_datetime().strftime("%Y-%m-%dT%H:%M:%S.%f")[:-3]  # Format UTC time
+        row[0] = gps_time1.to_datetime().strftime("%Y-%m-%dT%H:MM:SS.sss")  # Format UTC time
         row[1] = f"{float(row[2])}"  # Format Decimal Latitude
         row[2] = f"{float(row[3])}"  # Format Decimal Longitude
-        row[3] = ""  # Format Altitude above Start Point
-        # Replace the GPS week and time columns with the GPSTime converted to a datetime object
-        # row[0] = gps_time1.to_datetime().strftime("%Y-%m-%dT%H:%M:%S.%f")[:-3]  # Format UTC time
-        # row[1] = row[2]  # Format Decimal Latitude
-        # row[2] = row[3]  # Format Decimal Longitude
-        # row[3] = ""
-
+        row[3] = f"{float(row[4])}"  # Format Altitude above Start Point
         # Write the updated row to the output CSV file
+        row = row[:len(row)-1]
         writer.writerow(row)
-
         # Print GPS Time and Datetime
         print(f"GPS Time: {gps_time1}")
         print(f"Datetime: {gps_time1.to_datetime()}")
