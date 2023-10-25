a31fffd394be4c8ee5777fd172f8ba019ae75cb5
Skyrats <93954688+SkyratsDrones@users.noreply.github.com>
2023-10-25 09:51:36 -0300

    undo rm

---
+++ final/stage_2/fase2.py
@@ -0,0 +1,187 @@
+import rospy
+import time
+import math
+import time
+
+#from actuator import Actuator
+from std_msgs.msg import String
+from mav import MAV2
+
+
+SIMULATION = True
+MAX_TIME_ON_BASE = 15
+
+
+baseA = [0, 2, 2]   #Mais próxima
+baseB = [2, 2, 4]
+baseC = [8, 8, 1]
+baseD = [0, 0, 0]
+baseE = [0, 0, 0]
+bases = [baseA, baseB, baseC, baseD, baseE]
+
+#leitura da base A dá direção das próximas (ifs)
+#lista bases não visitadas
+#teste: caso não leia, muda altura
+#ajustar descentralização no gotolocal
+
+class QRCodeReader():
+    def __init__(self) -> None:
+        self.qrcode = String()
+        self.qrcode_list = []
+        #rospy.init_node('sky_vision_barcode_analyzer', anonymous=False)
+        rospy.Subscriber('/sky_vision/down_cam/qrcode_read', String, self.callback)
+        self.found_qr = False
+
+    def callback(self, message):
+        self.qrcode = message.data
+        if self.qrcode not in self.qrcode_list:
+            self.qrcode_list.append(self.qrcode)
+        self.found_qr = True
+
+
+"""
+class drone():
+    def __init__(self) -> None:
+        rospy.init_node("teste")
+        
+        self.goal_pose = PoseStamped()
+        self.current_pose = Vector3(0,0,0)
+        self.target_vel = TwistStamped()
+        self.setpoint_pub = rospy.Publisher('mavros/setpoint_position/local', PoseStamped, queue_size=1)
+        self.vel_pub = rospy.Publisher('mavros/setpoint_velocity/cmd_vel', TwistStamped, queue_size=1)
+        self.takeoff_srv = rospy.ServiceProxy('/mavros/cmd/takeoff', CommandTOL)
+        self.qrcode_detector = QRCodeReader()
+        self.found = self.qrcode_detector.found_qr
+
+        rospy.Subscriber('mavros/local_position/pose', PoseStamped, self.pos_callback)
+
+    def takeoff(self, height):
+        rospy.loginfo("TAKING OFF...")
+        self.set_mode("GUIDED")
+        self.arm()
+        rospy.wait_for_service('/mavros/cmd/takeoff', 10)
+
+        rospy.sleep(1)
+        response = self.takeoff_srv(altitude=height)
+        
+        if response.success:
+            rospy.loginfo("Takeoff completed!")
+            return
+        else:
+            rospy.loginfo("Takeoff failed!")
+            return
+        
+    def set_position(self, x, y, z, yaw = None):
+        self.goal_pose.pose.position.x = float(x)
+        self.goal_pose.pose.position.y = float(y)
+        self.goal_pose.pose.position.z = float(z)
+        #self.goal_pose.header.frame_id = ""
+        # if yaw == None:
+        #     self.goal_pose.pose.orientation = self.drone_pose.pose.orientation
+        # else:
+        #[self.goal_pose.pose.orientation.x, 
+        #self.goal_pose.pose.orientation.y, 
+        #self.goal_pose.pose.orientation.z,
+        #self.goal_pose.pose.orientation.w] = quaternion_from_euler(0,0,yaw) #roll,pitch,yaw
+            #print("X: " + str(self.goal_pose)))
+
+        self.setpoint_pub.publish(self.goal_pose)
+    
+    def go_to_local(self, coordenadas, yaw = None, sleep_time=20):
+        # rospy.loginfo("Going towards local position: (" + str(goal_x) + ", " + str(goal_y) + ", " + str(goal_z) + "), with a yaw angle of: " + str(yaw))
+
+        init_time = now = time.time()
+        while not rospy.is_shutdown() and now-init_time < sleep_time:
+            self.set_position(coordenadas[0], coordenadas[1], coordenadas[2], yaw)
+            now = time.time()
+        
+        # rospy.loginfo("Arrived at requested position")
+    
+    def set_position(self, x, y, z, yaw = None):
+        self.goal_pose.pose.position.x = float(x)
+        self.goal_pose.pose.position.y = float(y)
+        self.goal_pose.pose.position.z = float(z)
+        #self.goal_pose.header.frame_id = ""
+        # if yaw == None:
+        #     self.goal_pose.pose.orientation = self.drone_pose.pose.orientation
+        # else:
+        [self.goal_pose.pose.orientation.x, 
+        self.goal_pose.pose.orientation.y, 
+        self.goal_pose.pose.orientation.z,
+        self.goal_pose.pose.orientation.w] = quaternion_from_euler(0,0,yaw) #roll,pitch,yaw
+            #print("X: " + str(self.goal_pose)))
+
+        self.setpoint_pub.publish(self.goal_pose)
+
+    # def go_to(self, x, y, z, des_velocity, tol):#uses Velocity
+    #     if abs(self.current_pose.x - x) > tol:
+    #         if x > self.current_pose.x:
+    #             self.target_vel.twist.linear.x = des_velocity
+    #         else:
+    #             self.target_vel.twist.linear.x = -des_velocity
+    #         self.vel_pub.publish(self.target_vel) 
+    #     else:
+    #         self.vel_pub.publish(TwistStamped())
+        
+
+    def pos_callback(self, msg):
+
+        
+        try:
+            self.current_pose.x = float(msg.pose.position.x)
+            self.current_pose.y = float(msg.pose.position.y)
+            self.current_pose.z = float(msg.pose.position.z)
+            print(self.current_pose)
+        except:
+            pass
+    
+"""
+
+def main():
+    rospy.init_node('mavbase2')
+    dr = MAV2()
+    qr = QRCodeReader()
+    z = 2
+    sleep = 5
+
+
+    try:
+        dr.takeoff(z)
+        rospy.sleep (7)
+        dr.change_auto_speed(1)
+        for base in bases:
+            dr.go_to_local(base, yaw=math.pi/2, sleep_time=2)
+
+            start = time.time()
+            while (not qr.found_qr and time.time() - start < MAX_TIME_ON_BASE):
+
+                try:
+
+                    print(start)
+
+                    print(time.time() - start)
+                    
+                    print("here")
+
+
+                except KeyboardInterrupt:
+                    print("A")
+                    return
+                
+                finally:
+                    time.sleep(1)
+            
+            qr.found = False
+
+                
+
+
+
+        dr.go_to_local([0,0,0], yaw=math.pi/2, sleep_time=2)
+        time.sleep(sleep)
+        dr.land()
+
+    except KeyboardInterrupt:
+        print("cabo")
+
+main()
---
+++ final/stage_3/stage_3.py
@@ -0,0 +1,140 @@
+import rospy
+import time
+import math
+import dronekit
+from pymavlink import mavutil
+import time
+
+#from actuator import Actuator
+from geometry_msgs.msg import TwistStamped, PoseStamped, Point, Vector3
+from std_msgs.msg import String, Header
+from mavros_msgs.msg import PositionTarget
+from tf.transformations import quaternion_from_euler, euler_from_quaternion
+
+TOL = 0.2
+
+class BarcodeAnalyzer():
+    
+    def __init__(self) -> None:
+        self.barcode = String()
+        self.barcode_list = []
+        #rospy.init_node('sky_vision_barcode_analyzer', anonymous=False)
+        
+        rospy.Subscriber('/sky_vision/down_cam/barcode_read', String, self.callback)
+    
+    def callback(self, message):
+        self.barcode = message.data
+        if self.barcode not in self.barcode_list:
+            self.barcode_list.append(self.barcode)
+
+class drone():
+    
+    def __init__(self) -> None:
+        rospy.init_node("teste")
+        
+        self.bc = BarcodeAnalyzer()
+        self.goal_pose = PoseStamped()
+        self.current_pose = PoseStamped()
+        self.setpoint_pub = rospy.Publisher('mavros/setpoint_position/local', PoseStamped, queue_size=1)
+        rospy.Subscriber('mavros/local_position/pose', PoseStamped, self.pos_callback)
+        print("initiating, goal pose:", self.goal_pose)
+        
+    def set_position(self, x, y, z, yaw = None):
+        self.goal_pose.pose.position.x = float(x)
+        self.goal_pose.pose.position.y = float(y)
+        self.goal_pose.pose.position.z = float(z)
+
+        self.setpoint_pub.publish(self.goal_pose)
+
+    def go_to(self, x, y, z, yaw = None):
+        
+        print("Going to:", x, y, z, yaw)
+        print("Currently at: ", self.current_pose)
+        
+        if(abs(x - self.current_pose.pose.position.x) < TOL and
+              abs(y - self.current_pose.pose.position.y) < TOL and
+              abs(z - self.current_pose.pose.position.z) < TOL):
+            return True
+        
+        self.set_position(x, y, z, yaw)
+        print(self.current_pose)
+        time.sleep(0.2)
+        return False
+        
+
+    def pos_callback(self, msg):
+
+        self.current_pose.pose.position.x = float(msg.pose.position.x)
+        self.current_pose.pose.position.y = float(msg.pose.position.y)
+        self.current_pose.pose.position.z = float(msg.pose.position.z)
+        
+        #print(self.current_pose)
+
+def main():
+    dr = drone()
+    time.sleep(1)
+    step = 1
+    x, y, z = 1, 5, 1
+
+    x_start, z_start = x, z
+    
+    while not rospy.is_shutdown():
+        
+        try:
+
+            dr.go_to(x,y,z)
+            
+            if step == 1:
+                if(dr.go_to(x, y, z)):
+                    step += 1
+                    
+            elif step == 2:
+                if(dr.go_to(x, y , z)):
+                    x += 0.5
+                    time.sleep(1)
+                    
+                    if(x > 5):
+                        x = x_start
+                        step += 1
+                        
+            elif step == 3:
+                if(dr.go_to(x, y , z)):
+                    z += 0.5
+                    
+                    if(z > 3):
+                        step += 1
+                    else:
+                        step = 2
+
+            else:
+                if(dr.go_to(0, 0 , 3)):
+                    print("END OF MISSION")
+                
+            
+
+                
+            """
+            print("looping")
+                    
+            point = [2, 6, 1]
+            
+            dr.go_to(point[0], point[1], point[2])
+            
+            print("second")
+            
+            for z in range(4):  # Realizar os movimentos de ida e volta 4 vezes
+                
+                dr.go_to(point[0], point[1], point[2] + z)
+                for x in range(5):
+                    
+                    dr.go_to(point[0] + x, point[1], point [2] + z)
+
+            dr.go_to(0, 0, 1)
+            """
+            
+        except KeyboardInterrupt:
+            print("Process killed by user.")
+        
+        
+
+main()
