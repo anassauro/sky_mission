7f75c7094735b0abbcdb47c16b9d4b0eb9c38bc1
b-andreoni <127281892+b-andreoni@users.noreply.github.com>
2024-01-10 18:46:31 -0300

    Add testing files

---
+++ teste_eixo.py
@@ -0,0 +1,39 @@
+import rospy
+import time
+import math
+import dronekit
+from pymavlink import mavutil
+import time
+
+#from actuator import Actuator
+from geometry_msgs.msg import TwistStamped, PoseStamped, Point, Vector3
+from mavros_msgs.srv import SetMode, CommandBool, CommandTOL, ParamSet
+from std_msgs.msg import String, Header
+from mavros_msgs.msg import PositionTarget
+from tf.transformations import quaternion_from_euler, euler_from_quaternion
+from ..mav import MAV2
+
+
+
+def main():
+    rospy.init_node('mavbase2')
+    dr = MAV2()
+    z = 2
+    sleep = 5
+    try:
+        dr.takeoff(z)
+        rospy.sleep(7)
+        dr.go_to_local([1, 0, z], yaw=math.pi/2, sleep_time=5)  #eixo X
+        time.sleep(sleep)
+        dr.go_to_local([0, 1, z], yaw=math.pi/2, sleep_time=2)  #eixo Y
+        time.sleep(sleep)
+        dr.go_to_local([0, 0, z+1], yaw=math.pi/2, sleep_time=2)  #eixo Z
+        time.sleep(sleep)
+        dr.go_to_local([0, 0, 0], yaw=math.pi/2, sleep_time=2)
+        time.sleep(sleep)
+        dr.land()
+    except KeyboardInterrupt:
+        print("foi")
+
+
+main()
---
+++ teste_quadrado.py
@@ -0,0 +1,42 @@
+import rospy
+import time
+import math
+import dronekit
+from pymavlink import mavutil
+import time
+
+#from actuator import Actuator
+from geometry_msgs.msg import TwistStamped, PoseStamped, Point, Vector3
+from mavros_msgs.srv import SetMode, CommandBool, CommandTOL, ParamSet
+from std_msgs.msg import String, Header
+from mavros_msgs.msg import PositionTarget
+from tf.transformations import quaternion_from_euler, euler_from_quaternion
+from ..mav import MAV2
+
+def main():
+    rospy.init_node('mavbase2')
+    dr = MAV2()
+    z = 2
+    sleep = 5
+    square_size = 1.5
+    tol_time = 15
+    try:
+        dr.takeoff(z)
+        rospy.sleep(5)
+        dr.go_to_local([-square_size, 0, 3])
+        time.sleep(5)
+        dr.go_to_local([-square_size, square_size, z])
+        time.sleep(5)
+        dr.go_to_local([0, square_size, z])
+        time.sleep(5) 
+        dr.go_to_local([0, 0, z])
+        time.sleep(10)
+        dr.go_to_local([0, 0, z+1])
+        time.sleep(10)
+        dr.land()
+
+    except KeyboardInterrupt:
+        print("foi")
+
+
+main()
