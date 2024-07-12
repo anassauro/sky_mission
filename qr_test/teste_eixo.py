63f44169dc2ce035907347656aa316e83b10428e
luisacereda <106029376+luisacereda@users.noreply.github.com>
2024-07-12 15:19:29 -0300

    Update teste_eixo.py

--- qr_test/teste_eixo.py
+++ qr_test/teste_eixo.py
@@ -18,7 +18,7 @@ from mav import MAV2
 def main():
     rospy.init_node('mavbase2')
     dr = MAV2()
-    z = 2
+    z = 1
     sleep = 5
     try:
         dr.takeoff(z)
@@ -27,7 +27,7 @@ def main():
         time.sleep(sleep)
         dr.go_to_local([0, 1, z], 0, sleep_time=2)  #eixo Y
         time.sleep(sleep)
-        dr.go_to_local([0, 0, z+1], 0, sleep_time=2)  #eixo Z
+        dr.go_to_local([0, 0, z+0.2], 0, sleep_time=2)  #eixo Z
         time.sleep(sleep)
         dr.go_to_local([0, 0, 0], 0, sleep_time=2)
         time.sleep(sleep)
