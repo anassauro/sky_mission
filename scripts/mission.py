0d0e1e0cbad3f81bcae25a21093c224a636195e5
Caio H. Freitas <hfcaio@gmail.com>
2024-07-17 14:15:44 -0300

    communication.py testado

--- scripts/mission.py
+++ scripts/mission.py
@@ -1,7 +1,22 @@
 from sky_mission.sky_utils.communication import Mav
+import rospy
+
+def main():
+    mav = Mav(debug=True)
+
+    rospy.loginfo("Taking off...")
+
+    if not mav.takeoff(5):
+        print("Couldn't takeoff.")
+    else:
+        rospy.sleep(6)
+    
+    rospy.loginfo("Trying to land...")
+    if not mav.land():
+        rospy.loginfo("Couldnt't land.")
 
 if __name__ == "__main__":
     try:
-        Mav()
+        main()
     except:
         pass
