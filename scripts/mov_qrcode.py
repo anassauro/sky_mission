cbcb18ee7aa1b36d2bb8fa1ecd875ef972b682d9
hmoyen <helenamoyen@usp.br>
2024-07-18 02:59:35 -0300

    compatible with communication.py

--- scripts/mov_qrcode.py
+++ scripts/mov_qrcode.py
@@ -6,7 +6,7 @@ from pymavlink import mavutil
 import time
 import numpy as np
 
-from actuator import Actuator
+# from actuator import Actuator
 from geometry_msgs.msg import TwistStamped, PoseStamped, Point, Vector3
 from mavros_msgs.srv import SetMode, CommandBool, CommandTOL, ParamSet
 from std_msgs.msg import String, Header
@@ -34,44 +34,47 @@ class codeDetector():
 
 def main():
     rospy.init_node('mav')
-    dr = Mav()
+    dr = Mav(debug=True)
     qr = codeDetector()
-    dr.change_auto_speed(1)
+    # dr.change_auto_speed(1) # We do not have function for it yet
     z = 1,5
     x, y, z = 1, 1, 0     #QR Code distance from takeoff
 
     try:
         dr.takeoff(z)
         rospy.sleep(5)
-        dr.go_to_local([x, y,z])
+        dr.goto(x, y,z)
         rospy.sleep(5)
         if (not qr.found_qr):
-            dr.go_to_local([x, y, z - 0.2])
+            dr.goto(x, y, z - 0.2)
             rospy.sleep(5)
-        if (not qr.found_qr):
             print ("Not found")
             
         else:
             print("QR Code found!")
-            print("QR Code list:", qr.qrcode_list)
-            if qr.code[-1] == 1:
-                if qr.code[0] == "E":
-                    dr.go_to_local([x,y-1,z])
-                    rospy.sleep(5)
-                if qr.code[0] == "S":
-                    dr.go_to_local([x-1,y,z])
-                    rospy.sleep(5)
-                if qr.code[0] == "N":
-                    dr.go_to_local([x+1,y,z])
-                    rospy.sleep(5)
-                if qr.code[0] == "W":
-                    dr.go_to_local([x,y+1,z])
-                    rospy.sleep(5)
-                else:
-                    print("Sem Movimento")         
-            qr.found_qr = False
+            print("QR Code list:", qr.code_list)
+            # if qr.code[-1] == 1:
+            if qr.code[0] == "E":
+                dr.goto(x,y-1,z)
+                print("Moving east")
+                rospy.sleep(5)
+            elif qr.code[0] == "S":
+                dr.goto(x-1,y,z)
+                print("Moving south")
+                rospy.sleep(5)
+            elif qr.code[0] == "N":
+                dr.goto(x+1,y,z)
+                print("Moving north")
+                rospy.sleep(5)
+            elif qr.code[0] == "W":
+                dr.goto(x,y+1,z)
+                print("Moving west")
+                rospy.sleep(5)
+            else:
+                print("Sem Movimento")         
+        qr.found_qr = False
  
-        dr.go_to_local([0,0,z])
+        dr.goto(0,0,z)
         rospy.sleep(5)
         dr.land()
         
